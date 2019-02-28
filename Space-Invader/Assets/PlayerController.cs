999649831e692292b4dd4e58e745c38616390257
martin <plessis.du.martin@gmail.com>
2019-02-28 21:44:51 +0200

    1.7 Complete
    Added projectile, need to delete off-screen projectile gameObjects

--- Space-Invader/Assets/PlayerController.cs
+++ Space-Invader/Assets/PlayerController.cs
@@ -8,13 +8,38 @@ public class Boundary
     public float xMin, xMax, zMin, zMax;
 }
 
-
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Params")]
     public float speed;
     public float tilt;
     public Boundary boundary;
 
+    [Header("Projectile Params")]
+    public GameObject projectile;
+    public Transform projectileSpawnPoint;
+    public float fireDelta = 0.5F;
+    private float nextFire = 0.5F;
+    //private GameObject newProjectile;
+    private float myTime = 0.0F;
+
+
+    void Update()
+    {
+        myTime += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            //newProjectile = 
+            Instantiate(projectile, projectileSpawnPoint.position, projectileSpawnPoint.rotation);// as GameObject;
+
+            // create code here that animates the newProjectile
+
+            nextFire -= myTime;
+            myTime = 0.0F;
+        }
+    }
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
